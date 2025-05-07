522ea32a73becc5b188dad611a49d3ff84c16f4d
Restu Edo Setiaji <restuedosetiaji@gmail.com>
2025-05-07 23:50:24 +0700

    feat: use uuid as primary key in users table

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,6 +3,8 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
@@ -16,6 +18,10 @@ class User extends Authenticatable
     use HasFactory, Notifiable;
     use HasRoles;
     use SoftDeletes;
+    use HasUuids;
+
+    public $incrementing = false;
+    protected $keyType = 'string';
 
     /**
      * The attributes that are mass assignable.
