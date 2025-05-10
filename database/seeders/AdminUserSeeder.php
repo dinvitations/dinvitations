1a99a782ab2a7433cdbb98c3954b1d0ee02590f8
Restu Edo Setiaji <restuedosetiaji@gmail.com>
2025-05-10 20:31:23 +0700

    chore: set has_access to true for admin user in AdminUserSeeder

--- database/seeders/AdminUserSeeder.php
+++ database/seeders/AdminUserSeeder.php
@@ -18,6 +18,7 @@ class AdminUserSeeder extends Seeder
             [
                 'name' => 'Admin',
                 'password' => Hash::make('p4s5w0rd'),
+                'has_access' => true,
             ]
         );
 
