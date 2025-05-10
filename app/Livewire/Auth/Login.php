149d83053a35e95dc2f5fcfd048f0d0ed28dcca2
Restu Edo Setiaji <restuedosetiaji@gmail.com>
2025-05-10 20:30:32 +0700

    fix: prevent Alpine $store.sidebar error by removing SPA-style redirect

--- app/Livewire/Auth/Login.php
+++ app/Livewire/Auth/Login.php
@@ -51,7 +51,7 @@ class Login extends Component
             default           => '/',
         };
 
-        $this->redirectIntended(default: $redirectRoute, navigate: true);
+        $this->redirectIntended(default: $redirectRoute);
     }
 
     /**
