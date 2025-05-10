707d93b8429b37e7ec1dfbc792e420ed4b909a58
Restu Edo Setiaji <restuedosetiaji@gmail.com>
2025-05-10 20:32:45 +0700

    feat: add ClientUser seeder

---
+++ database/seeders/ClientUserSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Role;
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class ClientUserSeeder extends Seeder
+{
+    public function run(): void
+    {
+        // Create or update the client user
+        $client = User::updateOrCreate(
+            ['email' => 'restuedosetiaji@gmail.com'],
+            [
+                'name' => 'Restu Edo Setiaji',
+                'password' => Hash::make('Edo998877!'),
+                'has_access' => false,
+            ]
+        );
+
+        // Create 'client' role if it doesn't exist
+        $clientRole = Role::firstOrCreate(['name' => 'client']);
+
+        // Assign the role to the client user
+        $client->assignRole($clientRole);
+    }
+}
