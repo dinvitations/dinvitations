2130742b0fadeaddd6832134a440e529dd227773
Restu Edo Setiaji <restuedosetiaji@gmail.com>
2025-05-12 06:49:34 +0700

    feat: add migration for events table

---
+++ database/migrations/2025_05_11_231651_create_events_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->uuid('order_id');
+            $table->uuid('template_id')->nullable();
+            $table->string('name')->index();
+            $table->string('slug')->unique();
+            $table->timestamp('date_start');
+            $table->timestamp('date_end')->nullable();
+            $table->text('whatsapp_message')->nullable();
+            $table->text('location');
+            $table->string('location_latlong')->nullable();
+            $table->timestamp('published_at')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->foreign('order_id')
+                  ->references('id')->on('orders')
+                  ->onUpdate('cascade')
+                  ->onDelete('cascade');
+
+            $table->foreign('template_id')
+                  ->references('id')->on('templates')
+                  ->onUpdate('cascade')
+                  ->onDelete('set null');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('events');
+    }
+};
