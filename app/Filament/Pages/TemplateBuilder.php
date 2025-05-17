84df14624ba419a78c66f2dcfa6ca99a4d079728
Restu Edo Setiaji <restuedosetiaji@gmail.com>
2025-05-18 06:29:32 +0700

    refactor: remove cluster and move Template page

---
+++ app/Filament/Pages/TemplateBuilder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Filament\Pages;
+
+use Filament\Pages\Page;
+
+class TemplateBuilder extends Page
+{
+    protected static ?string $navigationIcon = 'heroicon-o-squares-2x2';
+
+    protected static ?string $navigationGroup = 'Shop';
+
+    protected static ?string $navigationLabel = 'Template';
+
+    protected static ?int $navigationSort = 1;
+
+    protected static ?string $slug = 'template-builder';
+    
+    protected static string $view = 'filament.pages.templates.template-builder';
+
+}
