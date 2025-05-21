d076cdfb0b5a5ff4b3a994af8120f02f88e55dcb
Restu Edo Setiaji <restuedosetiaji@gmail.com>
2025-05-21 20:40:59 +0700

    refactor: use label instead of resource lang for logout label

--- resources/lang/vendor/filament-panels/en/layout.php
+++ resources/lang/vendor/filament-panels/en/layout.php
@@ -2,17 +2,7 @@
 
 return [
 
-    'direction' => 'ltr',
-
     'actions' => [
-
-        'billing' => [
-            'label' => 'Manage subscription',
-        ],
-
-        'logout' => [
-            'label' => 'Logout',
-        ],
 
         'modal' => [
 
@@ -24,56 +14,12 @@ return [
                 'label' => 'Cancel'
             ],
 
-            'logout' => [
+            'submit' => [
                 'label' => 'Logout'
             ]
 
         ],
 
-        'open_database_notifications' => [
-            'label' => 'Open notifications',
-        ],
-
-        'open_user_menu' => [
-            'label' => 'User menu',
-        ],
-
-        'sidebar' => [
-
-            'collapse' => [
-                'label' => 'Collapse sidebar',
-            ],
-
-            'expand' => [
-                'label' => 'Expand sidebar',
-            ],
-
-        ],
-
-        'theme_switcher' => [
-
-            'dark' => [
-                'label' => 'Enable dark theme',
-            ],
-
-            'light' => [
-                'label' => 'Enable light theme',
-            ],
-
-            'system' => [
-                'label' => 'Enable system theme',
-            ],
-
-        ],
-
-    ],
-
-    'avatar' => [
-        'alt' => 'Avatar of :name',
-    ],
-
-    'logo' => [
-        'alt' => ':name logo',
-    ],
+    ]
 
 ];
