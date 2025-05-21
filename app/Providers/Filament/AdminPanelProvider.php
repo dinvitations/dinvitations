1e1c67ab30ca42e2d152614a4195e701698521dd
Restu Edo Setiaji <restuedosetiaji@gmail.com>
2025-05-21 20:40:59 +0700

    refactor: use label instead of resource lang for logout label

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -35,6 +35,7 @@ class AdminPanelProvider extends PanelProvider
             ->homeUrl(fn (): string => Dashboard::getUrl())
             ->userMenuItems([
                 'logout' => MenuItem::make()
+                    ->label('Logout')
                     ->icon('heroicon-o-arrow-right-on-rectangle')
                     ->color('danger'),
                 'telescope' => MenuItem::make()
