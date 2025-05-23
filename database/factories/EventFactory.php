2609df18e0f02b6becd6a049c3c4d8f8a33a7f0c
Restu Edo Setiaji <restuedosetiaji@gmail.com>
2025-05-23 16:15:13 +0700

    feat: add event model factory seeder

---
+++ database/factories/EventFactory.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Package;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Event>
+ */
+class EventFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        /* Use this to create a random package if needed */
+        // $package = Package::inRandomOrder()->first() ?? Package::factory()->create();
+
+        $package = null;
+
+        return [
+            'name' => Str::title(fake()->word()),
+            'description' => fake()->sentence(),
+            'package_id' => $package?->id,
+        ];
+    }
+}
