41eee7454c7aedf2f1674bb81aa7c16cabf72052
Restu Edo Setiaji <restuedosetiaji@gmail.com>
2025-05-24 19:29:45 +0700

    feat: add custom theme for filament pages

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
             input: [
                 'resources/css/app.css',
                 'resources/js/app.js',
-        tailwindcss(),
+                'resources/css/filament/web/theme.css',
             ],
             refresh: [
                 ...refreshPaths,
