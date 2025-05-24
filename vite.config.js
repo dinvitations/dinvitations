fc6b554d7b65f834e6c255cdf7aaf396feb793ba
Restu Edo Setiaji <restuedosetiaji@gmail.com>
2025-05-24 19:28:46 +0700

    feat: add tailwindcss for livewire views & extra class attribute

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,21 @@
 import {
     defineConfig
 } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import tailwindcss from "@tailwindcss/vite";
+import laravel, { refreshPaths } from 'laravel-vite-plugin';
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js',
         tailwindcss(),
+            ],
+            refresh: [
+                ...refreshPaths,
+                'app/Livewire/**',
+            ],
+        }),
     ],
     server: {
         cors: true,
