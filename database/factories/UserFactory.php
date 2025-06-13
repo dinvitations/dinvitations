6be456864a3e17249bfa1d85d381db091dc0fbe3
Restu Edo Setiaji <restuedosetiaji@gmail.com>
2025-06-13 18:20:29 +0700

    refactor: update user factory organizer_id

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\Order;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
@@ -24,12 +25,18 @@ class UserFactory extends Factory
      */
     public function definition(): array
     {
+        $organizer = User::inRandomOrder()
+            ->whereDoesntHave('roles', function ($query) {
+                $query->where('name', 'client');
+            })->first();
+
         return [
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
+            'organizer_id' => $organizer?->id
         ];
     }
 
