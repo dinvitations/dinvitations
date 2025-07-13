2c6432d3770a80b068b22d7d0eff6447a75fc08f
Restu Edo Setiaji <restuedosetiaji@gmail.com>
2025-07-13 19:22:42 +0700

    fix: use iframe for invitation template preview

--- resources/views/livewire/embed/show-invitation.blade.php
+++ resources/views/livewire/embed/show-invitation.blade.php
@@ -1,3 +1,32 @@
-<div>
-    @livewire('show-invitation', ['slug' => $slug])
+<style>
+  .resizable-wrapper {
+    width: 100%;
+    overflow: hidden;
+    display: flex;
+    justify-content: center;
+    border: 1px solid #eee;
+  }
+
+  .iframe-container {
+    width: 480px;
+    height: 720px;
+    resize: horizontal;
+    overflow: auto;
+    border: 1px solid #ccc;
+    min-width: 480px;
+    max-width: 100%;
+    box-sizing: border-box;
+  }
+
+  .iframe-container iframe {
+    width: 100%;
+    height: 100%;
+    border: none;
+  }
+</style>
+
+<div class="resizable-wrapper">
+  <div class="iframe-container">
+    <iframe src="{{ route('invitation.show', ['slug' => $slug]) }}"></iframe>
+  </div>
 </div>
